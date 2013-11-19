639c400ee3617158a6509bc88d1c72b862eb58f9
sektionschef <the_stevee@hotmail.com>
2013-11-19 22:32:41 +0100

    setup

---
+++ credentials/credentials.js
@@ -0,0 +1,8 @@
+var credentials = {
+	consumer_key: "XXXXXXXXX",
+	consumer_secret: "XXXXXXXXXXXXX",
+	access_token: "XXXXXXXXX",
+	access_token_secret: "XXXXXXXXXXXXXXXXX"
+};
+
+module.exports = credentials;
